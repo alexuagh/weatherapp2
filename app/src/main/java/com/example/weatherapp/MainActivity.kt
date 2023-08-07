48ee3b48336405fa1b4c96c0c70c0559d26b4a28
Alex Zaporoshchuk <105975560+alexuagh@users.noreply.github.com>
2023-08-07 21:00:44 +0300

    weatherapp2

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.weatherapp
+
+import android.graphics.drawable.shapes.Shape
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Indication
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+            .padding(end = 20.dp)
+            .width(width = 30.dp)
+            .height(height = 40.dp)
+
+    )
+
+    Button(onClick = { }) {
+
+        Icon(
+            imageVector = Icons.Filled.Favorite, contentDescription = null, modifier = Modifier
+                .padding(end = 18.dp)
+                .width(width = 20.dp)
+                .height(height = 20.dp)
+        )
+
+        @Composable
+        fun OneText(modifier: Modifier = Modifier) {
+            Row(modifier = modifier) {
+                Text(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 4.dp)
+                        .wrapContentWidth(Alignment.Start),
+                    text = ("Welcome")
+                )
+            }
+            Text("My friend")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
