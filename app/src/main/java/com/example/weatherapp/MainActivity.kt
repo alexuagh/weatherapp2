1f4d9ec20e0f693543fedf5bc49dec42fb19d892
Alex Zaporoshchuk <105975560+alexuagh@users.noreply.github.com>
2023-08-08 23:08:15 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -5,6 +5,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Indication
 import androidx.compose.foundation.background
@@ -37,7 +43,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -45,11 +54,11 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.min
 import com.example.weatherapp.ui.theme.WeatherAppTheme
-
 
 
 class MainActivity : ComponentActivity() {
@@ -62,26 +71,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column (modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.Black)) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.Black)
+                    ) {
                         Button(
                             onClick = { },
                             modifier = Modifier.fillMaxWidth()
                         ) {
                             Text(text = "Update weather")
-                            AnimatedVisibility(visible = ) {
-                                
                             }
-                        }
-                            Text(text = "Temperature:")
-                            Text(text = "Humidity:")
-                            Text(text = "Location:")
 
+                        CircularProgressIndicator (
+                            progress = 0.75,
+                            modifier = Modifier.fillMaxSize(),
+                            startAngle = 0,
+                            indicatorColor = MaterialTheme.colorScheme.secondary,
+                            trackColor = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.1f),
+                            strokeWidth = 4.dp
+                        )
+                        Text(text = "Temperature:")
+                        Text(text = "Humidity:")
+                        Text(text = "Location:")
                     }
-
                 }
             }
         }
     }
 }
+
